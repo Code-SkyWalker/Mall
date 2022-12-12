db1fa2529319b9df00b99afb856f4977b25ea322
Code-SkyWalker <916420794@qq.com>
2022-12-12 16:20:26 +0800

    添加Gateway

---
+++ mall-gateway/src/main/java/com/skywalker/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.skywalker.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Author Code SkyWalker
+ * @Date 22:33 2022/12/5
+ * @Classname GatewayApplication
+ * @Description TODO
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}

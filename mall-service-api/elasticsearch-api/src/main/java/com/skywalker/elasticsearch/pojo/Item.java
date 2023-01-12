3e3775b6966123a3f2564c849da5a4c4b7eb9cc2
Code-SkyWalker <916420794@qq.com>
2023-01-12 18:08:10 +0800

    test

---
+++ mall-service-api/elasticsearch-api/src/main/java/com/skywalker/elasticsearch/pojo/Item.java
@@ -0,0 +1,34 @@
+package com.skywalker.elasticsearch.pojo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+/**
+ * @Author Code SkyWalker
+ * @Date 11:29 2023/1/12
+ * @Classname Item
+ * @Description TODO
+ */
+@Document(indexName = "item")
+public class Item {
+
+    @Id
+    private Long id;
+
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    private String title; //标题
+
+    @Field(type = FieldType.Keyword)
+    private String category;// 分类
+
+    @Field(type = FieldType.Keyword)
+    private String brand; // 品牌
+
+    @Field(type = FieldType.Double)
+    private Double price; // 价格
+
+    @Field(index = false, type = FieldType.Keyword)
+    private String images; // 图片地址
+}
